7d59d0b9b8532631bc3e8218a8b15a86a3c27f11
siyuan-li <siyuanli0185@gmail.com>
2017-03-18 00:33:53 -0700

    commit miss file

--- client/src/app/components/editor/editor.component.ts
+++ client/src/app/components/editor/editor.component.ts
@@ -14,7 +14,7 @@ export class EditorComponent implements OnInit {
   defaultContent = {
     'Java': `public class Solution {
     
-    public int[] twoSum(int[] numbers, int target) {
+    public static void main(String[] args) {
         
     }
 }`,
